cdbed86527c6f4d5a05b74f5cfb43c565bd07b99
mz038197 <mz038197@gmail.com>
2026-01-26 01:55:58 +0800

    Update .gitignore to exclude credentials.json and add new dependencies to requirements.txt for Google Sheets API and data processing.

---
+++ google_sheet.py
@@ -0,0 +1,23 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_file(
+    "credentials.json",
+    scopes=scope
+)
+
+gc = gspread.authorize(creds)
+
+
+sheet = gc.open("python_test")
+worksheet = sheet.sheet1 
+# 取得所有資料
+all_data = worksheet.get_all_values()
+print(all_data)
+
+worksheet.append_row(["姓名", "年齡", "成績"])
